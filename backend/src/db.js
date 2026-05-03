0feda1c26bddf0ad78e77ab4136607ef4aa98f3b
Ravikargeti2003 <kargetiravi87@gmail.com>
2026-05-03 16:33:18 +0530

    fix db init timing for railway

--- backend/src/db.js
+++ backend/src/db.js
@@ -14,6 +14,50 @@
 
 // module.exports = pool;
 
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+
+// async function createPool() {
+//   const tempConn = await mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 3306,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//   });
+//   await tempConn.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``);
+//   await tempConn.end();
+
+//   return mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 3306,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//   });
+// }
+
+// // const poolPromise = createPool();
+
+// let poolPromise;
+
+// function getPool() {
+//   if (!poolPromise) {
+//     poolPromise = createPool();
+//   }
+//   return poolPromise;
+// }
+
+// module.exports = new Proxy({}, {
+//   get(_, prop) {
+//     return (...args) => poolPromise.then(pool => pool[prop](...args));
+//   }
+// });
+
+
+
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
@@ -24,6 +68,7 @@ async function createPool() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   });
+
   await tempConn.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``);
   await tempConn.end();
 
@@ -39,10 +84,20 @@ async function createPool() {
   });
 }
 
-const poolPromise = createPool();
+let poolPromise;
+
+function getPool() {
+  if (!poolPromise) {
+    poolPromise = createPool();
+  }
+  return poolPromise;
+}
 
 module.exports = new Proxy({}, {
   get(_, prop) {
-    return (...args) => poolPromise.then(pool => pool[prop](...args));
+    return async (...args) => {
+      const pool = await getPool();
+      return pool[prop](...args);
+    };
   }
 });
